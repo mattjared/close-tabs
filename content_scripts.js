3e5c40d57732c863c4f4e8f4021c89b2aaa5ff36
Matt Jared <matt.jared@vercel.com>
2023-05-02 19:54:52 -0500

    Clarence Glover

--- content_scripts.js
+++ content_scripts.js
@@ -1,12 +1,5 @@
-// const closeTabs = () => Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Redirecting to your Notion app…')) !== undefined && window.close();
-// const closeTabs = () => {
-//   console.log("this works");
-// }
-
 function closeTabs() {
   const element = Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Redirecting to your Notion app…'));
-  // console.log(element);
   (element !== undefined) ? window.close() : null;
 }
-// setTimeout(closeTabs, 60000);
 setTimeout(closeTabs, 10000);
