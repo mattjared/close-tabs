7919363917a1f77274f967264d361fc05f3da134
Matt Jared <matt.jared@vercel.com>
2023-05-02 19:54:29 -0500

    Anfernee Hardaway

--- content_scripts.js
+++ content_scripts.js
@@ -1,2 +1,12 @@
-const closeTabs = () => Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Redirecting to your Notion app…')) !== undefined && window.close();
-setTimeout(closeTabs, 60000);
+// const closeTabs = () => Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Redirecting to your Notion app…')) !== undefined && window.close();
+// const closeTabs = () => {
+//   console.log("this works");
+// }
+
+function closeTabs() {
+  const element = Array.from(document.querySelectorAll('div')).find(el => el.textContent.includes('Redirecting to your Notion app…'));
+  // console.log(element);
+  (element !== undefined) ? window.close() : null;
+}
+// setTimeout(closeTabs, 60000);
+setTimeout(closeTabs, 10000);
